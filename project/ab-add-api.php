2a968452553966721c9cd3fabf5ddf383b2fef14
NathanLai <a403440322@gmail.com>
2022-05-25 09:51:22 +0800

    add error msg

--- project/ab-add-api.php
+++ project/ab-add-api.php
@@ -4,9 +4,19 @@ require __DIR__. '/parts/connect_db.php';
 $output = [
     'success' => false,
     'postData' => $_POST,
+    'code' => 0,
     'error' => "", 
 ];
 // TODO 欄位檢查, 後端的檢查
+
+if(empty($_POST['name'])){
+    $output['error'] = '沒有姓名資料';
+    $output['code'] = 400;
+    echo json_encode($output, JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+
 
 $sql = "INSERT INTO `address_book`(
 `name`, `email`, `mobile`,
@@ -21,7 +31,7 @@ $stmt->execute([
     $_POST['name'],
     $_POST['email'],
     $_POST['mobile'],
-    $_POST['birthday'],
+    empty($_POST['birthday'])? NULL : $_POST['birthday'],
     $_POST['address'],
 ]);
 if($stmt->rowCount()==1){
